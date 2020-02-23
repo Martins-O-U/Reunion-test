3f1953634457369845c51e00af29ba12a7d557c5
HP <martinsonyedikachi@gmail.com>
2020-02-23 23:03:38 +0100

    edited home file display

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -4,10 +4,10 @@ import homeless from "./../../images/homeless.jpg"
 export default function Home() {
     return (
         <div className="container">
-            <div className="hero-background">
-                <img src={homeless} alt="Homeless Person" height="20%" />
+            <div className="home-background">
+                <img src={homeless} alt="Homeless Person" />
             </div>
-            <h1>This is home</h1>
+            <p>According to the United Nations in 2005, some 100 million persons were homeless worldwide, today that figure would have risen even more with unceasing wars and conflicts within the african sub regions </p>
         </div>
     )
 }
